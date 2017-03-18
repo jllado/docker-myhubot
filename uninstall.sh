1540272338faa04d1a3907665035e2186aba0004
Juan Lladó <jllado@gmail.com>
2017-03-18 13:20:09 +0100

    Used docker-compose to uninstall the containers

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-if [ $(docker ps -aq --filter name=dockermyhubot_hubot_1) ]; then
-    docker stop $(docker ps -aq --filter name=dockermyhubot_hubot_1)
-    docker rm -f $(docker ps -aq --filter name=dockermyhubot_hubot_1)
-fi
-if [ $(docker ps -aq --filter name=dockermyhubot_mongo_1) ]; then
-    docker stop $(docker ps -aq --filter name=dockermyhubot_mongo_1)
-    docker rm -f $(docker ps -aq --filter name=dockermyhubot_mongo_1)
-fi
+docker-compose down
 if [ $(docker images -aq --filter reference=dockermyhubot_hubot) ]; then
     docker rmi -f $(docker images -aq --filter reference=dockermyhubot_hubot)
 else
