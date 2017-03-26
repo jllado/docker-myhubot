d759efc3b2653eae4d223ae9cd5a855f95d5a9a4
Juan Lladó <jllado@gmail.com>
2017-03-26 21:06:43 +0200

    Reformat code

--- myhubot/scripts/daily_bookings.js
+++ myhubot/scripts/daily_bookings.js
@@ -10,35 +10,35 @@ var bookings_record = require('../domain/bookings_record.js');
 var travelc_user = process.env.TRAVELC_USER || 'user';
 var travelc_pass = process.env.TRAVELC_PASS || 'pass';
 
-module.exports = function(robot) {
-    var yesterday_bookings = function (room) {
-        robot.http('http://online.travelcompositor.com/resources/authentication/getAuthToken')
-            .query({'microsite': 'default', 'username': travelc_user, 'password': travelc_pass})
-            .header('Accept', 'application/json')
-            .get()(function(err, resp, body) {
-                let auth = JSON.parse(body);
-                let yesterday = clock.yesterday();
-                console.log('yesterday date: ' + yesterday);
-                robot.http('http://online.travelcompositor.com/resources/booking/getBookings')
-                .query({'from': yesterday, 'to': yesterday})
-                .header('Accept', 'application/json')
-                .header('auth-token', auth.token)
-                .get()(function(err, resp, body) {
-                    let bookings = JSON.parse(body);
-                    let bookings_count = bookings_mapper.count(bookings);
-                    robot.send({room: room}, phrase_builder.build(bookings_count));
-                    let record = bookings_record.get(robot);
-                    if (bookings_count > record) {
-                        robot.send({room: room}, '¡Hemos batido el record de reservas diarias! :muscle:');
-                        bookings_record.save(robot, bookings_count);
-                    }
-                });
-            });
-    };
-    cron.schedule('00 00 * * *', function() {
-        yesterday_bookings('-197523822'); //telegram group id
-    });
-    robot.hear(/yesterday bookings/, function(msg) {
-        yesterday_bookings(msg.envelope.message.room);
-    });
+module.exports = function (robot) {
+  var yesterday_bookings = function (room) {
+    robot.http('http://online.travelcompositor.com/resources/authentication/getAuthToken')
+      .query({'microsite': 'default', 'username': travelc_user, 'password': travelc_pass})
+      .header('Accept', 'application/json')
+      .get()(function (err, resp, body) {
+        let auth = JSON.parse(body);
+        let yesterday = clock.yesterday();
+        console.log('yesterday date: ' + yesterday);
+        robot.http('http://online.travelcompositor.com/resources/booking/getBookings')
+          .query({'from': yesterday, 'to': yesterday})
+          .header('Accept', 'application/json')
+          .header('auth-token', auth.token)
+          .get()(function (err, resp, body) {
+            let bookings = JSON.parse(body);
+            let bookings_count = bookings_mapper.count(bookings);
+            robot.send({room: room}, phrase_builder.build(bookings_count));
+            let record = bookings_record.get(robot);
+            if (bookings_count > record) {
+              robot.send({room: room}, '¡Hemos batido el record de reservas diarias! :muscle:');
+              bookings_record.save(robot, bookings_count);
+            }
+          });
+      });
+  };
+  cron.schedule('00 00 * * *', function () {
+    yesterday_bookings('-197523822'); //telegram group id
+  });
+  robot.hear(/yesterday bookings/, function (msg) {
+    yesterday_bookings(msg.envelope.message.room);
+  });
 }
