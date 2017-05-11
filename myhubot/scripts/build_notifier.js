fb4ea9bcb2b24c2cf68a56845e01f75075f29a64
Juan Lladó <jllado@gmail.com>
2017-05-11 23:08:38 +0200

    Optional channel parameter to build notification request

--- myhubot/scripts/build_notifier.js
+++ myhubot/scripts/build_notifier.js
@@ -15,10 +15,19 @@ module.exports = function (robot) {
     }
     return job + ': failed';
   };
-  robot.router.get("/build/:job/:status", function(req, res) {
+
+  var notify = function (robot, req, res) {
+    let channel = req.params.channel;
     let build_message = buildMessage(req.params.job, req.params.status);
-    room_notifier.send(robot, '-197645752', build_message);
+    room_notifier.send(robot, channel ? channel : '-197645752', build_message);
     res.status(200).send(build_message);
+  }
+
+  robot.router.get("/build/:job/:status", function(req, res) {
+    notify(robot, req, res);
+  });
+  robot.router.get("/build/:job/:status/:channel", function(req, res) {
+    notify(robot, req, res);
   });
 
 }
