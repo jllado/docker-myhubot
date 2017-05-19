59cea3e75aeeba9e85ea26625fe40e75d4c36bd5
Juan Lladó <jllado@gmail.com>
2017-05-19 20:29:53 +0200

    Added repository url to push notification

--- myhubot/scripts/bitbucket_notificar.js
+++ myhubot/scripts/bitbucket_notificar.js
@@ -7,7 +7,8 @@ var room_notifier = require('../domain/room_notifier.js');
 module.exports = function (robot) {
 
   robot.router.post('/bitbucket/push', function(req, res) {
-    room_notifier.send(robot, '-197523822', 'New commits in ' + req.body.repository.name);
+    let repository = req.body.repository;
+    room_notifier.send(robot, '-197523822', 'New commits in ' + repository.name + ': ' + repository.links.html.href);
     res.status(200).send('OK');
   });
 
